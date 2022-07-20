ee9d9679eea762c97663d7e7059dadce45b351b2
Aniket Maurya <theaniketmaurya@gmail.com>
2022-07-20 19:51:36 +0530

    cloudcompute cpu medium

--- research_app/components/model_demo.py
+++ research_app/components/model_demo.py
@@ -1,9 +1,7 @@
-from loguru import logger
-import os
 import gradio as gr
-from lightning.app.components.serve import ServeGradio
-
+import lightning as L
 from lightning.app import BuildConfig
+from lightning.app.components.serve import ServeGradio
 
 
 class CustomBuildConfig(BuildConfig):
@@ -13,27 +11,29 @@ class CustomBuildConfig(BuildConfig):
             "cd research_app/components && pip install ."
         ]
 
+
 class ModelDemo(ServeGradio):
     """Serve model with Gradio UI.
 
     You need to define i. `build_model` and ii. `predict` method and Lightning `ServeGradio` component will
     automatically launch the Gradio interface.
     """
-    inputs=[gr.inputs.Image(type='pil'), "textbox"]
-    outputs=[gr.outputs.Image(type='numpy'), 'text']
+    inputs = [gr.inputs.Image(type='pil'), "textbox"]
+    outputs = [gr.outputs.Image(type='numpy'), 'text']
     enable_queue = True
     examples = [['test.jpeg', 'what color is the left car?'],
                 ['test.jpeg', 'which region does the text " a grey car " describe?']]
 
     def __init__(self):
-        super().__init__(parallel=True, cloud_build_config=CustomBuildConfig())
+        super().__init__(parallel=True, cloud_build_config=CustomBuildConfig(),
+            cloud_compute=L.CloudCompute("cpu-medium"))
 
     def build_model(self):
         import os
         os.chdir("research_app/components/OFA")
         from .OFA.gradio_app import general_interface
-        
+
         return general_interface
-        
+
     def predict(self, image, instruction):
         return self.model(image, instruction)
